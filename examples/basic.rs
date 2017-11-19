2afbf28df0dffc8ec736385b6bcee1aa05c1e0fe
TheHellBox <hellboxstudio@yandex.ru>
2017-11-19 12:00:42 +0500

    Fix 2

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,6 +1,6 @@
-extern crate x11-screenshot;
+extern crate x11_screenshot;
 fn main() {
-    let screen = x11-screenshot::Screen::new();
+    let screen = x11_screenshot::Screen::new();
     let frame = screen.cap_frame(1920,1080,0,0); //ScrW, ScrH, PosX, PosY
     frame.save("./a.png").unwrap(); // Save image
     //Image docs http://www.piston.rs/image/image/index.html
