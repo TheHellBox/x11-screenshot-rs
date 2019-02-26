1c0659fe56131ed2a05f906b23a3c05865d7f45f
crumblingstatue <radiantstatue@gmail.com>
2019-02-26 15:45:27 +0100

    Format with rustfmt

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,7 +1,7 @@
 extern crate x11_screenshot;
 fn main() {
     let screen = x11_screenshot::Screen::new();
-    let frame = screen.cap_frame(1920,1080,0,0); //ScrW, ScrH, PosX, PosY
+    let frame = screen.cap_frame(1920, 1080, 0, 0); //ScrW, ScrH, PosX, PosY
     frame.save("./a.png").unwrap(); // Save image
-    //Image docs http://www.piston.rs/image/image/index.html
+                                    // Image docs http://www.piston.rs/image/image/index.html
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 #![allow(dead_code)]
 
-extern crate x11;
-extern crate libc;
 extern crate image;
+extern crate libc;
+extern crate x11;
+use self::image::{Pixel, Rgb, RgbImage};
 use self::libc::{c_int, c_ulong};
 use std::ffi::CString;
+use std::{ptr, slice};
 use x11::xlib;
-use std::{slice, ptr};
-use self::image::{RgbImage, Rgb, Pixel};
 pub struct Screen {
     display: *mut xlib::Display,
-    window: xlib::Window
+    window: xlib::Window,
 }
 #[derive(Debug)]
 pub struct bgr {
@@ -21,44 +21,58 @@ pub struct bgr {
 }
 
 impl Screen {
-    pub fn new() -> Screen{
+    pub fn new() -> Screen {
         unsafe {
             let display = xlib::XOpenDisplay(ptr::null());
             let screen = xlib::XDefaultScreen(display);
             let root = xlib::XRootWindow(display, screen);
-            Screen{
+            Screen {
                 display: display,
-                window: root
+                window: root,
             }
         }
     }
-    pub fn cap_frame(&self, w: u32, h: u32, x: i32, y: i32) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>>{
+    pub fn cap_frame(
+        &self,
+        w: u32,
+        h: u32,
+        x: i32,
+        y: i32,
+    ) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
         let mut img = unsafe {
-        xlib::XGetImage(self.display,
-                        self.window,
-                        x,
-                        y,
-                        w,
-                        h,
-                        !1 as c_ulong,
-                        2 as c_int)
+            xlib::XGetImage(
+                self.display,
+                self.window,
+                x,
+                y,
+                w,
+                h,
+                !1 as c_ulong,
+                2 as c_int,
+            )
         };
 
         let mut fullimg = RgbImage::new(w, h);
 
-        if(!img.is_null()){
+        if (!img.is_null()) {
             let image = unsafe { &mut *img };
-            let sl: &[bgr] = unsafe { slice::from_raw_parts((image).data as *const _,
-                (image).width as usize * (image).height as usize)};
+            let sl: &[bgr] = unsafe {
+                slice::from_raw_parts(
+                    (image).data as *const _,
+                    (image).width as usize * (image).height as usize,
+                )
+            };
             let clone = fullimg.clone();
             let mut pixs = clone.enumerate_pixels();
-            for mut x in sl{
-                let (xc,yc,p) = pixs.next().unwrap();
-                fullimg.put_pixel(xc,yc,*Rgb::from_slice(&[x.r,x.g,x.b]));
+            for mut x in sl {
+                let (xc, yc, p) = pixs.next().unwrap();
+                fullimg.put_pixel(xc, yc, *Rgb::from_slice(&[x.r, x.g, x.b]));
             }
         }
 
-        unsafe {xlib::XDestroyImage(img as *mut _);}
+        unsafe {
+            xlib::XDestroyImage(img as *mut _);
+        }
         fullimg
     }
 }
