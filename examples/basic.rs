945f32363e2470054c6f96d60f5a09733a76c271
crumblingstatue <radiantstatue@gmail.com>
2019-02-26 16:55:33 +0100

    Rename a.png in example to example_screenshot.png, add it to .gitignore

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,6 +2,7 @@ extern crate x11_screenshot;
 fn main() {
     let screen = x11_screenshot::Screen::new();
     let frame = screen.capture();
-    frame.save("./a.png").unwrap(); // Save image
-                                    // Image docs http://www.piston.rs/image/image/index.html
+    // Save image
+    // For documentation on the image crate, see http://www.piston.rs/image/image/index.html
+    frame.save("example_screenshot.png").unwrap();
 }
